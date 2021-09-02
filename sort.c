2eda01bd120793b856798e612385d29417dee4d3
Benivator <tayl1042@purdue.edu>
2021-09-02 10:53:48 -0400

    Does this work>

--- sort.c
+++ sort.c
@@ -4,4 +4,4 @@ void ssort(int * arr, int size) {
 	/* For step 3, fill this in to perform a selection sort
 	   For step 4, add conditional compilation flags to perform an ascending selection sort instead */
 }
-// please god work
+// please god work!!
